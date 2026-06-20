a6dc0ece289d959d84e4611b874384d1cff7ce7d
Krigjo25 <krigjo25@gmail.com>
2026-06-20 09:08:30 +0200

    refactor(config): migrate vite config to ts & enhance type safety.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import { configDefaults } from 'vitest/config'
+
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
